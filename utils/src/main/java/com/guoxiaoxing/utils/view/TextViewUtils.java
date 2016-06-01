7fdb700e4565d73012ea4ac1c8e2102697e9bc5b
guoxiaoxing <guoxiaoxing@souche.com>
2016-06-01 16:50:08 +0800

    TextView工具类

---
+++ utils/src/main/java/com/guoxiaoxing/utils/view/TextViewUtils.java
@@ -0,0 +1,38 @@
+package com.guoxiaoxing.utils.view;
+
+/**
+ * Author: guoxiaoxing
+ * Email: guoxiaoxingv@163.com
+ * Site: https://github.com/guoxiaoxing
+ * Date: 16/6/1 下午4:42
+ * Function: TextView工具类
+ * <p>
+ * Modification history:
+ * Date                 Author              Version             Description
+ * --------------------------------------------------------------------------
+ * 16/6/1 下午4:42      guoxiaoxing          1.0
+ */
+public class TextViewUtils {
+
+    /**
+     * TextView自动滚动
+     * <TextView
+     *   android:id="@+id/textView"
+     *   android:layout_width="match_parent"
+     *   android:layout_height="wrap_content"
+     *   android:ellipsize="marquee"
+     *   android:marqueeRepeatLimit="marquee_forever"
+     *   android:scrollHorizontally="true"
+     *   android:singleLine="true"
+     *   android:text="【不添加省略号，当被选中时滚动显示文字】"
+     *   android:textColor="#FF000000"
+     *   android:textSize="30dp" />
+     *
+     *   文字滚动
+     *   findViewById(R.id.textView).setSelected(true);
+     *
+     *   停止滚动
+     *   findViewById(R.id.textView).setSelected(false);
+     */
+
+}
