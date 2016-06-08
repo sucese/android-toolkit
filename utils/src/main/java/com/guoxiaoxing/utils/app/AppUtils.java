1bd8d7ebc20585a4319e8f31d4c1519e07cb274c
guoxiaoxing <guoxiaoxing@souche.com>
2016-06-08 14:27:33 +0800

    工具拆分

--- utils/src/main/java/com/guoxiaoxing/utils/app/AppUtils.java
+++ utils/src/main/java/com/guoxiaoxing/utils/app/AppUtils.java
@@ -60,7 +60,7 @@ public class AppUtils {
      * @param context 上下文
      * @return 当前版本Code
      */
-    public static int getVerCode(Context context) {
+    public static int getVersionCode(Context context) {
         int verCode = -1;
         try {
             String packageName = context.getPackageName();
@@ -78,7 +78,7 @@ public class AppUtils {
      * @param context 上下文
      * @return 当前版本信息
      */
-    public static String getVerName(Context context) {
+    public static String getVersionName(Context context) {
         String verName = "";
         try {
             String packageName = context.getPackageName();
@@ -126,51 +126,9 @@ public class AppUtils {
      * @return 最大内存
      */
     public static long getMaxMemory() {
-
         return Runtime.getRuntime().maxMemory() / 1024;
     }
 
-    /**
-     * 安装apk
-     *
-     * @param context 上下文
-     * @param file    APK文件
-     */
-    public static void installApk(Context context, File file) {
-        Intent intent = new Intent();
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setDataAndType(Uri.fromFile(file),
-                "application/vnd.android.package-archive");
-        context.startActivity(intent);
-    }
-
-    /**
-     * 安装apk
-     *
-     * @param context 上下文
-     * @param file    APK文件uri
-     */
-    public static void installApk(Context context, Uri file) {
-        Intent intent = new Intent();
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.setAction(Intent.ACTION_VIEW);
-        intent.setDataAndType(file, "application/vnd.android.package-archive");
-        context.startActivity(intent);
-    }
-
-    /**
-     * 卸载apk
-     *
-     * @param context     上下文
-     * @param packageName 包名
-     */
-    public static void uninstallApk(Context context, String packageName) {
-        Intent intent = new Intent(Intent.ACTION_DELETE);
-        Uri packageURI = Uri.parse("package:" + packageName);
-        intent.setData(packageURI);
-        context.startActivity(intent);
-    }
 
     /**
      * 检测服务是否运行
@@ -291,54 +249,6 @@ public class AppUtils {
             }
         }
         return false;
-    }
-
-    /**
-     * 获取应用签名
-     *
-     * @param context 上下文
-     * @param pkgName 包名
-     * @return 返回应用的签名
-     */
-    public static String getSign(Context context, String pkgName) {
-        try {
-            PackageInfo pis = context.getPackageManager()
-                    .getPackageInfo(pkgName,
-                            PackageManager.GET_SIGNATURES);
-            return hexdigest(pis.signatures[0].toByteArray());
-        } catch (NameNotFoundException e) {
-            e.printStackTrace();
-            return null;
-        }
-
-    }
-
-    /**
-     * 将签名字符串转换成需要的32位签名
-     *
-     * @param paramArrayOfByte 签名byte数组
-     * @return 32位签名字符串
-     */
-    private static String hexdigest(byte[] paramArrayOfByte) {
-        final char[] hexDigits = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 97,
-                98, 99, 100, 101, 102};
-        try {
-            MessageDigest localMessageDigest = MessageDigest.getInstance("MD5");
-            localMessageDigest.update(paramArrayOfByte);
-            byte[] arrayOfByte = localMessageDigest.digest();
-            char[] arrayOfChar = new char[32];
-            for (int i = 0, j = 0; ; i++, j++) {
-                if (i >= 16) {
-                    return new String(arrayOfChar);
-                }
-                int k = arrayOfByte[i];
-                arrayOfChar[j] = hexDigits[(0xF & k >>> 4)];
-                arrayOfChar[++j] = hexDigits[(k & 0xF)];
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "";
     }
 
     /**
