32f57bc520329d68d651b80b4ad59a6731eecb34
guoxiaoxing <guoxiaoxing@souche.com>
2016-08-19 14:41:24 +0800

    add logger

--- demo/src/main/java/com/guoxiaoxing/demo/MainActivity.java
+++ demo/src/main/java/com/guoxiaoxing/demo/MainActivity.java
@@ -1,20 +1,21 @@
 package com.guoxiaoxing.demo;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
-
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    @Bind(R.id.rv_list)
-    RecyclerView mRvList;
+    @Bind(R.id.btn_log_activity)
+    Button mBtnLog;
 
     private Context mContext;
     private ArrayList<String> mList = new ArrayList<>();
@@ -24,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        initData();
+        setupView();
+        setupData();
     }
 
     @Override
@@ -34,10 +35,26 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.unbind(this);
     }
 
-    private void initData(){
+    private void setupView() {
+        mBtnLog.setOnClickListener(this);
+    }
+
+    private void setupData() {
         mContext = MainActivity.this;
 
         mList.add("CacheActivity");
         mList.add("BitmapActivity");
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn_log_activity:
+                startActivity(new Intent(mContext, LogActivity.class));
+                break;
+        }
+
     }
 }
