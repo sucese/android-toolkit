82759351eb495b1ba20f645d3b3e47228c31456b
guoxiaoxing <guoxiaoxingse@gmail.com>
2017-11-23 18:42:54 +0800

    feature: add new util

---
+++ library/src/main/java/com/guoxiaoxing/toolkit/common/MemoryConstants.java
@@ -0,0 +1,39 @@
+package com.guoxiaoxing.toolkit.common;
+
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 存储相关常量
+ *
+ * For more information, you can visit https://github.com/guoxiaoxing or contact me by
+ * guoxiaoxingse@163.com.
+ *
+ * @author guoxiaoxing
+ */
+public final class MemoryConstants {
+
+    /**
+     * Byte与Byte的倍数
+     */
+    public static final int BYTE = 1;
+    /**
+     * KB与Byte的倍数
+     */
+    public static final int KB   = 1024;
+    /**
+     * MB与Byte的倍数
+     */
+    public static final int MB   = 1048576;
+    /**
+     * GB与Byte的倍数
+     */
+    public static final int GB   = 1073741824;
+
+    @IntDef({BYTE, KB, MB, GB})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Unit {
+    }
+}
---
+++ library/src/main/java/com/guoxiaoxing/toolkit/common/TimeConstants.java
@@ -0,0 +1,43 @@
+package com.guoxiaoxing.toolkit.common;
+
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 时间相关常量
+ *
+ * For more information, you can visit https://github.com/guoxiaoxing or contact me by
+ * guoxiaoxingse@163.com.
+ *
+ * @author guoxiaoxing
+ */
+public final class TimeConstants {
+
+    /**
+     * 毫秒与毫秒的倍数
+     */
+    public static final int MSEC = 1;
+    /**
+     * 秒与毫秒的倍数
+     */
+    public static final int SEC  = 1000;
+    /**
+     * 分与毫秒的倍数
+     */
+    public static final int MIN  = 60000;
+    /**
+     * 时与毫秒的倍数
+     */
+    public static final int HOUR = 3600000;
+    /**
+     * 天与毫秒的倍数
+     */
+    public static final int DAY  = 86400000;
+
+    @IntDef({MSEC, SEC, MIN, HOUR, DAY})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Unit {
+    }
+}
